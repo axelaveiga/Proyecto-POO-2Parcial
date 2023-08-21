817155feacfb986799de4a85b7c2c3161af2ff46
dayanara5 <dnbone@espol.edu.ec>
2023-08-21 02:25:55 -0500

    Implementación de la interfaz buscar vehiculo

--- src/main/java/ec/edu/espol/proyecto/Buscar_vehiculoController.java
+++ src/main/java/ec/edu/espol/proyecto/Buscar_vehiculoController.java
@@ -4,9 +4,19 @@
  */
 package ec.edu.espol.proyecto;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.stage.Stage;
 
 /**
  * FXML Controller class
@@ -14,13 +24,47 @@ import javafx.fxml.Initializable;
  * @author dayan
  */
 public class Buscar_vehiculoController implements Initializable {
+   @FXML
+    private Button btRetroceder;
+    @FXML
+    private ImageView imageview15;
+    @FXML
+    private ImageView imageview16;
 
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
+        visualimagen();
         // TODO
-    }    
+    }
+        @FXML
+    private void retroceder(ActionEvent event) {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/ec/edu/espol/proyecto/menu_vehiculo.fxml"));
+            Parent root = loader.load();
+
+            Scene scene = new Scene(root);
+            Stage stage = new Stage();
+
+            stage.setScene(scene);
+            stage.show();
+
+            Stage mystage = (Stage) this.btRetroceder.getScene().getWindow();
+            mystage.close();
+
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+    }
     
+    @FXML
+    private void visualimagen() {
+        Image image15 = new Image("/imagenes/flecha.png");
+        imageview15.setImage(image15);
+        Image image16 = new Image("/imagenes/rosalogo.png");
+        imageview16.setImage(image16);
+    }
+
 }
